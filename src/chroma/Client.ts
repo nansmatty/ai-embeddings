4a2741c7baf3602c406e1e711eb44c961ba63495
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-06-24 21:47:21 +0530

    basic implementation of chroma db

---
+++ src/chroma/Client.ts
@@ -0,0 +1,25 @@
+import { ChromaClient } from 'chromadb';
+
+const client = new ChromaClient({
+	host: 'localhost',
+	port: 8000,
+});
+
+async function main() {
+	const response = await client.createCollection({ name: 'data-test' });
+
+	console.log(response);
+}
+
+async function addData() {
+	const collection = await client.getCollection({ name: 'data-test' });
+	const result = await collection.add({
+		ids: ['id1'],
+		documents: ['Here is my entry'],
+		embeddings: [[0.1, 0.2]],
+	});
+
+	console.log(result);
+}
+
+addData();
